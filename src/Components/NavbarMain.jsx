64db675d33f62053e87c75687f8d5e8de58157f9
Richard Webber <rikki.webber@gmail.com>
2023-12-31 18:37:23 -0700

    shopping cart image added to cart

--- src/Components/NavbarMain.jsx
+++ src/Components/NavbarMain.jsx
@@ -1,5 +1,6 @@
 import { Container, Nav, NavDropdown, Navbar } from "react-bootstrap";
 import { Link } from 'react-router-dom'
+import React from "react";
 // import styles from "./Cart.css"
 
 
@@ -26,16 +27,8 @@ export default function NavbarMain() {
                         <Link to='/shop' className='nav-link'>Shop</Link>
                         <Link to='/archive' className='nav-link'>Archive</Link>
                         <Link to='/account' className="nav-link">Account</Link>
-                        <Link to='/cart' className='nav-link'> Cart
-                        {/* <div className={styles.navbar__cart}>
-                            <h3 className={styles.cart__title}>Cart</h3>
-                                <img
-                                    className={styles.cart__image}
-                                    src="https://image.flaticon.com/icons/svg/102/102276.svg"
-                                    alt="shopping cart"
-                                />
-                            <div className={styles.cart__counter}>{cartCount}</div>
-                        </div>   */}
+                        <Link to='/cart' className='nav-link'> 
+                            <i className="material-icons">shopping_cart</i>
                         </Link>
                     </Nav>
                 </Navbar.Collapse>
