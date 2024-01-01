7322ee0af36366e6f0cc9b60755f013c7d243419
Richard Webber <rikki.webber@gmail.com>
2023-12-31 18:17:56 -0700

    Working on building out useState for Cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom'
-import NavbarMain from './Components/NavbarMain'
+import NavbarMain from './Components/NavbarMain.jsx'
+import Footer from './Components/Footer.jsx'
 
 export default function App() {
     return (
         <>
         <NavbarMain />
         <Outlet />
+        <Footer />
         </>
     )
 }
--- src/Components/NavbarMain.jsx
+++ src/Components/NavbarMain.jsx
@@ -1,9 +1,20 @@
 import { Container, Nav, NavDropdown, Navbar } from "react-bootstrap";
 import { Link } from 'react-router-dom'
-
+// import styles from "./Cart.css"
 
 
 export default function NavbarMain() {
+    // const [cartCount, setCartCount] = useState(0)
+
+    // useEffect(() => {
+    //     let count = 0
+    //     cart.forEach((item) => {
+    //         count += item.qty
+    //     });
+
+    //     setCartCount(count)
+    // }, [cart, Cartcount])
+
     return (
         <Navbar expand='md' bg='dark' data-bs-theme='dark'>
             <Container fluid>
@@ -15,7 +26,17 @@ export default function NavbarMain() {
                         <Link to='/shop' className='nav-link'>Shop</Link>
                         <Link to='/archive' className='nav-link'>Archive</Link>
                         <Link to='/account' className="nav-link">Account</Link>
-                        <Link to='/cart' className='nav-link'>Cart</Link>
+                        <Link to='/cart' className='nav-link'> Cart
+                        {/* <div className={styles.navbar__cart}>
+                            <h3 className={styles.cart__title}>Cart</h3>
+                                <img
+                                    className={styles.cart__image}
+                                    src="https://image.flaticon.com/icons/svg/102/102276.svg"
+                                    alt="shopping cart"
+                                />
+                            <div className={styles.cart__counter}>{cartCount}</div>
+                        </div>   */}
+                        </Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
