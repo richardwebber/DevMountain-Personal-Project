f353ec8328f7c0850776a335cfb93e733e8bd4bc
Richard Webber <rikki.webber@gmail.com>
2024-01-04 16:33:48 -0700

    save point for the day

--- src/Components/IndividualProductPage.jsx
+++ src/Components/IndividualProductPage.jsx
@@ -2,6 +2,13 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import './ProductPage.css'
+import Col from 'react-bootstrap/Col'
+import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image';
+import Row from 'react-bootstrap/Row';
+
+
 
 const IndividualProductPage = () => {
 
@@ -22,10 +29,20 @@ const IndividualProductPage = () => {
 
     console.log('Rendering IndividualProductPage for: ', product.name)
     return (
-        <div>
-            <h2>{product.name}</h2>
-            <p>{product.description}</p>
-        </div>
+        // <div className="productDiv">
+
+        //     <h2>{product.name}</h2>
+        //     <p>{product.description}</p>
+        // </div>
+        <Container>
+            <Row>
+                <Col>
+                    <Image /> 
+                    <h2>{product.name}</h2>
+                    <p>{product.description}</p>
+                </Col>
+            </Row>
+        </Container>
     )
 }
 
